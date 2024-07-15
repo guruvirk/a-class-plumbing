bf87c1d2334b542b0120a11a4db915e1293442d8
Karan Mishra <karan.mishra@fretron.com>
2024-07-15 16:56:24 +0530

    Contact us form added

--- script.js
+++ script.js
@@ -35,6 +35,77 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelector(".testimonial-slider-right")
     .addEventListener("click", slideRight);
 
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const name = document.getElementById("name").value;
+      const subject = document.getElementById("subject").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      const html = `
+            <h4>Contact - A Class Plumbing.</h4>
+            <p><b>Name: </b> ${name}</p>
+            <p><b>Subject: </b> ${subject}</p>
+            <p><b>Email: </b> ${email}</p>
+            <p><b>Message: </b> ${message}</p>
+            <br>
+        `;
+
+      const data = {
+        sender: {
+          name: "Name",
+          email: "info@domain.ca",
+        },
+        to: [
+          {
+            email: "to@domain.ca",
+            name: "A Class Plumbing Admin",
+          },
+        ],
+        subject: "New Contact Request",
+        htmlContent: html,
+      };
+
+      fetch("https://api.brevo.com/v3/smtp/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+          "api-key": "api-key",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((result) => {
+          console.log("Success:", result);
+          // Assuming you have an endpoint to handle database insertion
+          const xhr = new XMLHttpRequest();
+          xhr.open("POST", "insert-contact.php", true);
+          xhr.setRequestHeader(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+          );
+          xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+              console.log("Contact saved to database.");
+              window.location.href = "success-contact.html";
+            } else if (xhr.readyState === 4) {
+              window.location.href = "fail.html";
+            }
+          };
+          xhr.send(
+            `name=${name}&subject=${subject}&email=${email}&message=${message}`
+          );
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          window.location.href = "fail.html";
+        });
+    });
+
   // Initialize slider
   showSlides();
 });
